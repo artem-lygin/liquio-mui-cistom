1d5d70d0051674d4cd68fc1e8cd8a31e7dff3cd1
Andrii Arsenin <a.arsenin@kitsoft.ua>
2026-07-29 08:09:05 -0400

    feat(plugin-sdk): export TaskPaymentProvider
    Wires the new base class into the package's public API. @liquio/plugin-sdk
    needs a version bump/republish before packages/task-payone-plugin can
    depend on it for real.

--- packages/plugin-sdk/src/index.ts
+++ packages/plugin-sdk/src/index.ts
@@ -2,6 +2,7 @@ export * from "./manifest/plugin_manifest";
 export * from "./providers/base_provider";
 export * from "./providers/event_external_service_provider";
 export * from "./providers/external_reader_provider";
+export * from "./providers/task_payment_provider";
 export * from "./loader/types";
 export * from "./loader/plugin_registry";
 export * from "./loader/plugin_loader";
